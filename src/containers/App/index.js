992c37f35e31bd2e3c35b449239adad65555902c
Celso Fabri Junior <celso.fabri@live.com>
2021-04-29 22:30:48 -0300

    feat: initial structure config

---
+++ src/containers/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <p>API Key: <strong>c496e90670f92991911d2271103f3c48</strong></p>
+    </div>
+  );
+}
+
+export default App;
